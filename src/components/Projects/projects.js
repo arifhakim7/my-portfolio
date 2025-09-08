c95bbc407741084e14564c4b2d2db47da20b4d13
arifhakim7 <arifhakimasri@gmail.com>
2025-09-08 10:26:01 +0800

    add new project

--- src/components/Projects/projects.js
+++ src/components/Projects/projects.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './projects.css';
 import Diabetease from '../../assets/diabetease.png';
+import TicTacToe from '../../assets/tictactoe.png';
 
 const Projects = () => {
   return (
@@ -37,9 +38,35 @@ const Projects = () => {
           </div>
           </div>
         </div>
+        <div className="row align-items-center p-4">
+          {/* Image */}
+          <div className="col-md-6 text-center mb-4 mb-md-0">
+            <div className="project-img-wrapper">
+              <img src={TicTacToe} alt="TicTacToe" className="img-fluid project-img" />
+            </div>
+          </div>
 
-
-        
+          {/* Text */}
+          <div className="col-md-6">
+            <h2 className="project-title">
+              <span className="fw-bold">TicTacToe</span><br />
+            </h2>
+            <p className="project-desc">
+              An interactive web-based Tic-Tac-Toe game where users can play against each other or against the computer. 
+              Designed with a clean UI and responsive layout for both desktop and mobile devices.
+            </p>
+            <div className="project-links mt-4">
+            <a
+              href="https://github.com/arifhakim7/TicTacToe"
+              className="project-link"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              LEARN MORE
+            </a>
+          </div>
+          </div>
+        </div>
       </div>
     </section>
   );
